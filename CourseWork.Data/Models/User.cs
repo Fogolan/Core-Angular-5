38ac44bbd3b0c5ea0cc3f753e59e659544bd0dc7
Yurij Bogdan <fogolan@gmail.com>
2018-04-02 00:02:24 +0300

    user model

---
+++ CourseWork.Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PartyPlanner.Data.Models
+{
+    public class User
+    {
+      public int Id { get; set; }
+      public string IdentityId { get; set; }
+      public UserIdentity Identity { get; set; }
+    }
+}
