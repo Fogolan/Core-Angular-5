280af05c8f153836dbb5696cc78a76cb88e48f20
Unknown <tanyusha_shkoda@mail.ru>
2018-05-05 19:55:55 +0300

    create models

--- CourseWork.Data/Entities/PartyPlannerContext.cs
+++ CourseWork.Data/Entities/PartyPlannerContext.cs
@@ -10,7 +10,32 @@ namespace PartyPlanner.Data.Entities
         : base(options)
       {
       }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Recipe>()
+                .HasKey(ct => new { ct.CocktailId, ct.IngredientId });
 
-      public DbSet<User> Users { get; set; }
+            modelBuilder.Entity<Recipe>()
+                .HasOne(ct => ct.Cocktail)
+                .WithMany(c => c.Recipes)
+                .HasForeignKey(ct => ct.CocktailId);
+
+            modelBuilder.Entity<Recipe>()
+                .HasOne(ct => ct.Ingredient)
+                .WithMany(t => t.Recipes)
+                .HasForeignKey(ct => ct.IngredientId);
+
+        }
+
+
+        public DbSet<Cocktail> Cocktails { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ CourseWork.Data/Models/Cocktail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyPlanner.Data.Models
+{
+    public class Cocktail
+    {
+        public Cocktail()
+        {
+            Recipes = new List<Recipe>();
+        }
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Degrees { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        [Required]
+        public bool Active { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public UserIdentity User { get; set; }
+
+        public ICollection<Recipe> Recipes { get; set; }
+    }
+}
