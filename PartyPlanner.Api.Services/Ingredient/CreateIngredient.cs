f1b0bf814c4f4c9345b09c27987d22d446d3bde5
Unknown <tanyusha_shkoda@mail.ru>
2018-05-06 12:49:02 +0300

    added create and delete ingredient

---
+++ PartyPlanner.Api.Services/Ingredient/CreateIngredient.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using PartyPlanner.Data.Entities;
+
+namespace PartyPlanner.Api.Services.Ingredient
+{
+    public class CreateIngredient
+    {
+        public class Command : IRequest<int>
+        {
+            public Data.Models.Ingredient Ingredient { get; set; }
+        }
+
+        public class Handler : AsyncRequestHandler<Command, int>
+        {
+            private readonly PartyPlannerContext _context;
+
+            public Handler(PartyPlannerContext context)
+            {
+                _context = context;
+            }
+
+            protected override Task<int> HandleCore(Command command)
+            {
+                command.Ingredient.Active = true;
+                _context.Ingredients.Add(command.Ingredient);
+                _context.SaveChanges();
+
+                return Task.FromResult(command.Ingredient.Id);
+            }
+        }
+    }
+}
---
+++ PartyPlanner.Api.Services/Ingredient/GetRecipe.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartyPlanner.Api.Services.Ingredient
+{
+    class GetRecipe
+    {
+    }
+}
