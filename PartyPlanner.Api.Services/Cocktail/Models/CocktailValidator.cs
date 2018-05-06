dd88bc9a62dfaf8517a2a4c5411fae5ac4e56056
Unknown <tanyusha_shkoda@mail.ru>
2018-05-06 13:15:50 +0300

    added ingredient and cocktail validators

---
+++ PartyPlanner.Api.Services/Cocktail/Models/CocktailValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace PartyPlanner.Api.Services.Cocktail.Models
+{
+    public class CocktailValidator : AbstractValidator<Data.Models.Cocktail>
+    {
+        public CocktailValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull();
+            RuleFor(x => x.Active)
+                .NotNull();
+            RuleFor(x => x.Amount)
+                .NotNull()
+                .WithMessage("field Amount is required");
+            RuleFor(x => x.Amount)
+                .ExclusiveBetween(10, 5000)
+                .WithMessage("field Degrees must be at least 10 and not more than 5000");
+            RuleFor(x => x.CreatedDate)
+                .NotNull();
+            RuleFor(x => x.Degrees)
+                .NotNull()
+                .WithMessage("field Amount is required"); ;
+            RuleFor(x => x.Degrees)
+                .ExclusiveBetween(1, 100)
+                .WithMessage("field Degrees must be at least 1 and not more than 100");
+            RuleFor(x => x.Image)
+                .NotNull()
+                .WithMessage("field Amount is required");
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("field Amount is required");
+        }
+    }
+}
---
+++ PartyPlanner.Api.Services/Ingredient/Models/IngredientValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PartyPlanner.Api.Services.Ingredient.Models
+{
+    public class IngredientValidator : AbstractValidator<Data.Models.Ingredient>
+    {
+        public IngredientValidator()
+        {
+            RuleFor(x => x.Id);
+            RuleFor(x => x.Active)
+                .NotNull();
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("field Amount is required");
+        }
+    }
+}
