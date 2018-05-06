cee22e696370b5bee0b6e379603f66fd811da38d
Yura Bogdan <y2.bogdan@itransition.com>
2018-05-06 21:05:02 +0300

    fixed authorization

--- PartyPlanner.Api.Services/Auth/Models/Credentials.cs
+++ PartyPlanner.Api.Services/Auth/Models/Credentials.cs
@@ -1,15 +1,19 @@
 ﻿namespace PartyPlanner.Api.Services.Auth.Models
 {
-  public class Credentials
-  {
-    public string Username { get; set; }
+    public class Credentials
+    {
+        public string Username { get; set; }
 
-    public string Role { get; set; }
+        public string Role { get; set; }
 
-    public string Token { get; set; }
+        public string Token { get; set; }
 
-    public int ExpiresIn { get; set; }
+        public int ExpiresIn { get; set; }
 
-    public string Id { get; set; }
-  }
+        public string Id { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        public bool Active { get; set; }
+    }
 }
